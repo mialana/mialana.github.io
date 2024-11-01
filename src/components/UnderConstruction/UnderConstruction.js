ff904064d72080294fcab6eea9f71f2776109419
mialana <96770662+mialana@users.noreply.github.com>
2024-11-02 05:12:18 +0800

    added clockwork machine video

--- src/components/UnderConstruction/UnderConstruction.js
+++ src/components/UnderConstruction/UnderConstruction.js
@@ -1,17 +1,36 @@
 import React, { useContext } from "react";
+import styled from "styled-components";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { underConstructionData } from "../../data/underConstructionData";
-import "./UnderConstruction.css";
+
+import SneakPeek from "../../assets/mp4/sneak-peek.mp4";
 
 function UnderConstruction() {
   const { theme, _ } = useContext(ThemeContext);
 
-  
+  const Background = styled.div`
+    display: flex;
+    height: 100vh;
+    background: ${theme.quaternary};
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-family: "Poppins", sans-serif;
+  `;
+
+  const Message = styled.p`
+    width: 60vw;
+    text-align: center;
+    color: ${theme.tertiary};
+  `;
 
   return (
-    <div className="landing" style={{ backgroundColor: theme.quaternary }}>
-        <p>{underConstructionData.message}</p>
-    </div>
+    <Background>
+      <Message>{underConstructionData.message}</Message>
+      <video width="720" height="480" controls autoplay="true">
+        <source src={SneakPeek} type="video/mp4" />
+      </video>
+    </Background>
   );
 }
 
--- src/data/underConstructionData.js
+++ src/data/underConstructionData.js
@@ -1,3 +1,3 @@
 export const underConstructionData = {
-  message: "Hi I'm under construction",
+  message: "Hi you've caught me on an off day! I'm currently revamping this site. Feel free to explore what is here currently, but here is a cool new sneakpeek of the better visuals to come!",
 };
