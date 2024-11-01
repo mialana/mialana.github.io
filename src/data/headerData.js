8414ea29c117e7952b59c6cb050dd8c1fd5e9626
mialana <96770662+mialana@users.noreply.github.com>
2024-11-02 05:29:45 +0800

    update resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,5 +2,5 @@
 export const headerData = {
     name: 'Amy M. Liu',
     description: "Hi! My name is Amy. I'm currently pursuing my Bachelor's degree in Digital Media Design (3D Computer Programming) at the University of Pennsylvania in the exciting city of Philadelphia, PA.",
-    resumePdf: 'https://drive.google.com/file/d/1BUKGqsMUozRcHcDJSdsjtcNRtOpvi8f_/view?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1x5tstqjXA8adaF3F1cAZwJ8mK7fA01tW/view?usp=sharing'
 }
